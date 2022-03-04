24f8d90714121deb167eb3dc8b9fabb26ac53b82
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-04 04:08:51 -0500

    global vars

---
+++ zsh/conf/_globals.zsh
@@ -0,0 +1,18 @@
+export PATH="$PATH:./node_modules/.bin:$HOME/.local/bin"
+export TERM=alacritty
+export HISTFILE=$ZDOTDIR/history
+export EDITOR=nvim
+
+# locale
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# locations
+export HSH=/home/shared
+export XTRA=/mnt/xtra
+export LOCALAPPS=$HOME/.local/share
+
+# app-specific locations
+export GOPATH=$LOCALAPPS/go
+export ZPLUG=$ZDOTDIR/plugins
+
