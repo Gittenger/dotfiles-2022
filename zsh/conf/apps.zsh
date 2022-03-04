807522dca7467a265f1c4e11678db0a6ba91c42f
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-04 04:08:35 -0500

    initialize apps

---
+++ zsh/conf/apps.zsh
@@ -0,0 +1,18 @@
+export NVM_DIR="$HOME/.local/share/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/john/.local/share/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/john/.local/share/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/john/.local/share/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/john/.local/share/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
