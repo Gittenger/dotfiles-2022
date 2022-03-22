61145de5d9752d532b777de7466f38e288b1e4ad
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-03-22 19:39:52 -0400

    rm NVM, mv to shared

--- zsh/conf/apps.zsh
+++ zsh/conf/apps.zsh
@@ -1,6 +1,3 @@
-export NVM_DIR="$HOME/.local/share/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 __conda_setup="$('/home/john/.local/share/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
